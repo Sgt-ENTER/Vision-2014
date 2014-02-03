2d3aac002c8d4fb46ea215d7af8bfa01c4f8052b
Thomas Hebden <thomas.hebden98@gmail.com>
2014-02-03 20:25:59 +1100

    balltracking ready to be paired with the Beagle bone
    returns coordidnates of goals

--- Goal.py
+++ Goal.py
@@ -8,26 +8,29 @@ class GoalFinder:
         self._vc = cv2.VideoCapture(self.videoPort)
         self._width = 1.0 * width # Force a float
         self._height = 1.0 * height
-       
+
 		#video resolution
         self._vc.set(cv.CV_CAP_PROP_FRAME_WIDTH, self._width)
         self._vc.set(cv.CV_CAP_PROP_FRAME_HEIGHT, self._height)
-        
+
         #Set Video Capture properties
         self._vc.set(cv.CV_CAP_PROP_BRIGHTNESS, 0.2)
-        self._vc.set(cv.CV_CAP_PROP_SATURATION, 0.5)  
-        self._vc.set(cv.CV_CAP_PROP_CONTRAST, 0.5)  
-        
+        self._vc.set(cv.CV_CAP_PROP_SATURATION, 0.5)
+        self._vc.set(cv.CV_CAP_PROP_CONTRAST, 0.5)
+
+        self.rect_index = []
         # Public member variables to store the last calculated values
         # Invalid values indicate ball not found
-        self.xbar = 99.0 # xbar, ybar should be in the range [-1.0, 1.0]
-        self.ybar = 99.0
-        self.width = 99.0 # diam should be in the range (0.0, 2.0]
-        self.height = 99.0
-        
+##        self.xbar = 99.0 # xbar, ybar should be in the range [-1.0, 1.0]
+##        self.ybar = 99.0
+##        self.width = 99.0 # diam should be in the range (0.0, 2.0]
+##        self.height = 99.0
+        self.rectangles = []
+
+
         self.threshold = 250
         self.maxval = 255
-       
+
     def find(self):
         if not self._vc:
             # Try to reinitialise, but still return None
@@ -37,12 +40,12 @@ class GoalFinder:
         retval, frame = self._vc.read()
         if not retval:
             return None
-            
+
         # Do Goal Tracking Bit
         greyimage = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
         equ = cv2.equalizeHist(greyimage)
         ret, dogshit = cv2.threshold(equ, self.threshold, self.maxval, cv2.THRESH_BINARY)
-        dogshit2 = cv2.adaptiveThreshold(dogshit, self.maxval,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV,19,2)
+        dogshit2 = cv2.adaptiveThreshold(dogshit, self.maxval,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,19,2)
         kernel = cv2.getStructuringElement (cv2.MORPH_RECT,(5, 5))
         opened = cv2.morphologyEx(dogshit, cv2.MORPH_OPEN, kernel)
         blur = cv2.GaussianBlur(opened, (1,1), 0)
@@ -50,55 +53,62 @@ class GoalFinder:
         edge = cv2.Canny(blur,0, self.maxval)
         dilopened = cv2.dilate(dogshit, kernel, iterations = 2)
         #dilopened = cv2.erode(dogshit, kernel, iterations = 2)
-       # bgmask = self.bgsub.apply(frame)
-		 
+        #bgmask = self.bgsub.apply(frame)
+
         contours, hierarchy = cv2.findContours(dilopened, cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_NONE)
+        found_rectangles = []
         for index, contour in enumerate(contours):
-			area = cv2.contourArea(contour)
-			if area > 2750:
-				cv2.drawContours(frame, contours, index, (0,255,0),4)
-				print index
-        
+            #self.rect_index[index] = index
+            area = cv2.contourArea(contour)
+            if area > 1500:
+                x,y,w,h = cv2.boundingRect(contours[index])
+                found_rectangles.append([x,y,w,h])
+        self.rectangles = found_rectangles
+
         goal_found = False
-        #if contours: 
+        #if contours:
 			#for index, contour in enumerate(contours):
 				#largest_size = cv2.contourArea(contour)
 				#largest_index = index
-		
+
 			#if largest_index > 0:
-				#x,y,w,h = cv2.boundingRect(contours[largest_index])
-				#cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 7)
+
+#cv2.drawContours(frame, contours, index, (0,255,0),4)
 				#cv2.drawContours(frame, contours, index, (0,255,0))
 				#goal_found = True
-			
+
         if not goal_found:
             # No ball found so set the member variables to invalid values
-            self.xbar = 99.0
-            self.ybar = 99.0
-            self.height = 99.0
-            self.width = 99.0
-            
+##            self.xbar = 99.0
+##            self.ybar = 99.0
+##            self.height = 99.0
+##            self.width = 99.0
+
         # Return the frame, the contours and largest image in case we
         # want to show them on the screen
-        return 	dogshit2#, contours, largest_index
-    
+         return 	frame #self.rectangles#, contours, index#, contours, largest_index
+
     def absolute(self):
         # Convert xbar, ybar and diam to absolute values for showing on screen
         return (int((self.xbar+1.0)*self._width/2.0),
             int((self.ybar+1.0)*self._height/2.0),
             int(self.height*self._width/2.0))
-    
+
 if __name__ == "__main__":
     gf = GoalFinder( 640, 480)
     cv2.namedWindow("preview")
     while True:
         result = gf.find()
         frame = result
-       #frame, contours, largest_index = result 
-       #cv2.drawContours(frame, contours, largest_index, (0,255,0), 2)         
-        
+        for rect in gf.rectangles:
+            x,y,w,h = rect
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 7)
+        print index
+        #frame, contours, largest_index = result\
+        #cv2.drawContours(frame, contours, index, (0,255,0), 2)
+
         cv2.imshow("preview", frame)
-        
+
         key = cv2.waitKey (20)
         if key != -1: # Exit on any key
             break
