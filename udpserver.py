94c265ba91c803ec052595ec35d2a8a19d8ec3a7
James Ward <j.ward@acfr.usyd.edu.au>
2014-02-14 10:56:27 +1100

    Rewrite server to use UDP. No tabs in code.

---
+++ udpserver.py
@@ -0,0 +1,75 @@
+import socket
+import threading
+
+from ball import BallFinder
+from Goal import GoalFinder
+
+HOST, PORT = "", 4774 # Bind to all address on port 4774
+
+gf = GoalFinder()
+bf = BallFinder()
+
+ball_finding = threading.Event()
+goal_finding = threading.Event()
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((HOST, PORT)) # Any interface, port 4774
+
+def find_goal():
+    while True:
+        goal_finding.wait()
+        gf.find()
+        # Output a UDP packet to whoever is listening
+        response = '{} {} {}'.format(gf.xpos, gf.ypos, gf.angle)
+        
+def find_ball():
+    while True:
+        ball_finding.wait()
+        bf.find()
+        # Output a UDP packet to whoever is listening
+        response = '{} {} {}'.format(bf.xbar, bf.ybar, bf.diam)
+
+def get_mode():
+    '''Get the desired mode from the cRIO - whether to search for
+    the goal or a particular colour of ball.'''
+    data, addr = sock.recvfrom(8) # buffer size is 8 bytes
+    # The recvfrom call will block until data received, so we just wait
+    if data[0].lower() == 'g':
+        # We want the goal tracking thread
+        ball_finding.clear()
+        goal_finding.set()
+    else:
+        bf.setColour(data[0].lower())
+        #Catching thread
+        goal_finding.clear()
+        ball_finding.set()
+
+    
+if __name__ == "__main__":
+    # Start a thread with the server -- that thread will then start one
+    # more thread for each request
+    server_thread = threading.Thread(target=get_mode, name="udpserver")
+    # Exit the server thread when the main thread terminates
+    # Makes breaking out of program with Ctrl-C easier
+    server_thread.daemon = True
+    server_thread.start()
+    
+    goal_thread = threading.Thread(target=find_goal, name = 'finding goal')
+    goal_thread.daemon = True
+    goal_finding.clear()
+    goal_thread.start()
+    
+    ball_thread = threading.Thread(target=find_ball, name = 'finding ball')
+    ball_thread.daemon = True
+    ball_finding.clear()
+    ball_thread.start()
+    
+    while True:
+        pass
+        
+        # Infinite loop to keep the main thread running
+        # and waiting for connections
+        # Makes breaking out of program with Ctrl-C easier
+        #pass
+
