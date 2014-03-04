670e4a0a8e8ce27692275451e1e1b3f5315672d3
Abhinav Sureshkumar <askp@outlook.com>
2014-03-05 00:55:50 +1100

    added stable goal code - returns gRange,angle,Hot goal

--- goal.py
+++ goal.py
@@ -1,7 +1,6 @@
 #Import libraries
 import cv2
 import cv2.cv as cv
-import numpy as np
 import math as m
 
 #Camera number
@@ -9,10 +8,10 @@ CAMERA = 0
 
 class GoalFinder:
     def __init__(self, width = 640, height = 480): # Constructor to get the video capture set up
-		#video Capture settings
-        self.videoPort = 1 #change to index camera
+        self._vc = cv2.VideoCapture(CAMERA)
         self._width = 1.0 * width # Force a float
         self._height = 1.0 * height
+        self.center = 0.5* self._width
 
 		#initialise video feed and make sure its good
         try: 
@@ -29,24 +28,23 @@ class GoalFinder:
         self._vc.set(cv.CV_CAP_PROP_SATURATION, 0.5)
         self._vc.set(cv.CV_CAP_PROP_CONTRAST, 0.5)
 
-        # Detected rectangles info
-        self.rect_index = []
-        self.rectangles = []
+        #Default values
         self.defaultHvalue = 99.0
         self.rectHeight = self.defaultHvalue
 
         # Best position to fire from
-        self.bestPosition = 5 # ft (position from goal)
+        self.rect_index = []
+        self.gRange = self.defaultHvalue
         self.angle = self.defaultHvalue
-        self.xpos = self.defaultHvalue
-        self.ypos = self.defaultHvalue
-        self.currentpos = self.defaultHvalue
+        self.Hot = self.defaultHvalue
+        self.currentPos = self.defaultHvalue
+        self.currentWidth = self.defaultHvalue
 
         # Threshold to detect rectanlges
         self._threshold = 250
         self._maxval = 255
-        self._minarea = 1800
-        
+        self._minarea = 2.8125*self._width
+
         # Kernel for eroding/dilating
         self._kernel = cv2.getStructuringElement (cv2.MORPH_RECT,(5, 5))
 
@@ -54,19 +52,24 @@ class GoalFinder:
     def find(self):
         if not self._vc:
             # Try to reinitialise, but still return None
-            import time
-            time.sleep(0.01)
-            
             self.__init__()
             return None
         # We have a video capture object so we can proceed
         retval, frame = self._vc.read()
         if not retval:
+            self.gRange = self.defaultHvalue
+            self.angle = self.defaultHvalue
+            self.Hot = self.defaultHvalue
             return None
 
         # Do Goal Tracking Bit
+        #Convert to grey scale
         greyimage = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+        #Equalise to remove brightness
         equ = cv2.equalizeHist(greyimage)
+
+        # Convert to binary
         ret, thresh = cv2.threshold(equ, self._threshold, self._maxval, cv2.THRESH_BINARY)
         thresh2 = cv2.adaptiveThreshold(thresh, self._maxval,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,19,2)
         opened = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, self._kernel)
@@ -77,35 +80,53 @@ class GoalFinder:
         contours, hierarchy = cv2.findContours(dilopened, cv.CV_RETR_EXTERNAL, cv.CV_CHAIN_APPROX_NONE)
         found_rectangles = []
         filter_height = []
-        nothernstar = self.defaultHvalue
+        filter_x = []
+        xvalue = []
+        northernstar = self.defaultHvalue
+        goal_found = False
         for index, contour in enumerate(contours):
             area = cv2.contourArea(contour)
             if area > self._minarea:
                 x,y,w,h = cv2.boundingRect(contours[index])
                 found_rectangles.append([x,y,w,h])
                 filter_height.append([h])
-                filter_height = sorted(filter_height)
+                filter_x.append([x])
                 if len(filter_height) != 0:
-                   nothernstar = filter_height[len(filter_height)-1]
-        self.rectangles = found_rectangles
-        self.rectHeight = nothernstar
-        self.rect_index = len(found_rectangles)
+                    for index, w in enumerate(filter_height):
+                        if w == max(filter_height):
+                            xvalue = index - 1
 
-        #self.currentpos = (8*10**(-9)*(self.rectHeight**4))-(8*10**(-6)*(self.rectHeight**3))+(0.0032*(self.rectHeight**2))-(0.6061*self.rectHeight)+53.116
-        self.angle = m.acos(self.bestPosition/self.currentpos) #in radians
-        self.angle = (self.angle*m.pi/2)
-        self.xpos = self.currentpos*m.cos(self.angle)
-        self.ypos = self.currentpos*m.sin(self.angle)
+                    filter_height = sorted(filter_height)
+                    northernstar = filter_height[len(filter_height)-1]
+                    angletoS = filter_x[0]
+                    self.rectangles = found_rectangles	
+                    if northernstar != 0:
+                        self.rectHeight = northernstar[len(northernstar)-1]
+                        self.rectWidth = angletoS[len(angletoS)-1]
+                        self.rect_index = len(found_rectangles)
+
+                        self.rectWidth = abs(self.rectWidth - self.center)
+                        self.currentPos = 461.25*self.rectHeight**(-0.916) # in meters
+                        self.currentWidth = 150.56*abs(self.rectWidth)**(-0.805)
+                        self.angle = m.degrees(m.acos(-1 + (self.currentWidth**2/self.currentPos**2)))
+                        self.gRange = self.currentPos
+                        goal_found = True
+
+        if not goal_found:
+			self.gRange
+			self.angle
+			self.Hot
+
         return frame #self.rectangles#, contours, index#, contours, largest_index
 
     def absolute(self):
         # Convert xbar, ybar and diam to absolute values for showing on screen
-        return (int(self.xpos),
-            int(self.ypos),
-            int(self.angle))
+        return (float(self.gRange),
+            float(self.angle),
+            int(self.Hot))
 
 if __name__ == "__main__":
-    gf = GoalFinder( 640, 480)
+    gf = GoalFinder(640, 480)
     cv2.namedWindow("preview")
     while True:
         result = gf.find()
