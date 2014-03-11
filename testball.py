f8df6414450242a4b6b50f1f8753fb9da92b557f
James Ward <j.ward@acfr.usyd.edu.au>
2014-03-12 09:43:23 +1100

    Improved error messages for ball test program.

--- testball.py
+++ testball.py
@@ -19,9 +19,11 @@ class TestBall(unittest.TestCase):
                 frame = cv2.imread('img/'+row[0])
                 bf.setColour(row[1])
                 bf.find(frame)
-                self.assertAlmostEqual(bf.xbar, float(row[2]), delta=0.05)
-                self.assertAlmostEqual(bf.ybar, float(row[3]), delta=0.05)
-                self.assertAlmostEqual(bf.diam, float(row[4]), delta=0.05)
+                delta = 0.05
+                message = row[0] + " - %s\nExpected: %s +/- "+str(delta)+"\nReceived: %s"
+                self.assertAlmostEqual(bf.xbar, float(row[2]), delta=delta, msg=message % ("xbar", row[2], str(bf.xbar)))
+                self.assertAlmostEqual(bf.ybar, float(row[3]), delta=delta, msg=message % ("ybar", row[3], str(bf.ybar)))
+                self.assertAlmostEqual(bf.diam, float(row[4]), delta=delta, msg=message % ("diam", row[4], str(bf.diam)))
                 
 
 
